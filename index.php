0f215894f1008d6e157cf4976abba44c81f43126
小鱼yuzifu <yuzifu@yuzifu.top>
2023-04-04 21:33:34 +0800

    static

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+header('Access-Control-Allow-Origin:*');
+if (date("m.d") == "03.05") {
+    $path = "img_rice";
+}
+else {
+    $path = "img";
+}
+header("Content-type: image/jpeg");
+$url = './' . getRandomfile($filePath);
+readfile ($url);
+
+function getfilecounts($filePath)
+{
+    $dir = './' . $filePath;
+    $handle = opendir($dir);
+    $i = 0;
+    while (false !== $file = (readdir($handle))) {
+        if ($file !== '.' && $file != '..') {
+            $i++;
+        }
+    }
+    closedir($handle);
+    return $i;
+}
+
+function getRandomfile($filePath)
+{
+    $trueFileNum = rand(1, getfilecounts($filePath));
+    $dir = './' . $filePath;
+    $handle = opendir($dir);
+    $i = 0;
+    while (false !== $file = (readdir($handle))) {
+        if ($file !== '.' && $file != '..') {
+            $i++;
+            if ($i == $trueFileNum) {
+                $trueFile = $file;
+            }
+        }
+    }
+    closedir($handle);
+    return $filePath . "/" . $trueFile;
+}
+?>
